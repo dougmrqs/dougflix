34abfe1aac67d7aa360cb79bfc4480788458909f
Douglas Marques <douglas.bmarques@hotmail.com>
2020-07-28 12:30:14 -0300

    :sparkles: Carousel improvement

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -4,11 +4,22 @@ import { Link } from 'react-router-dom'
 
 export default () => (
     <BaseTemplate>
-        <h1>
-            Cadastro de Categoria
-        </h1>
+        <h1>Cadastro de Categoria</h1>
 
-        
+        <form>
+
+            <label>
+                Nome da Categoria:
+                <input
+                    type="text"
+                />
+            </label>
+
+            <button>
+                Cadastrar
+            </button>
+        </form>
+
         <Link to='/'>
             Ir para Home
         </Link>
